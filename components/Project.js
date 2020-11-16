91471737913b379855eb00933dde39bd9a76d5db
Falah Nurli Filano <falahnurli10@gmail.com>
2020-11-16 13:38:48 +0700

    add gestures

--- components/Project.js
+++ components/Project.js
@@ -1,24 +1,162 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+	Animated,
+	Dimensions,
+	TouchableWithoutFeedback,
+	StatusBar,
+	TouchableOpacity,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { connect } from 'react-redux';
+import { LinearGradient } from 'expo-linear-gradient';
+
+function mapStateToProps(state) {
+	return {
+		action: state.action,
+	};
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		openCard: () =>
+			dispatch({
+				type: 'OPEN_CARD',
+			}),
+		closeCard: () =>
+			dispatch({
+				type: 'CLOSE_CARD',
+			}),
+	};
+}
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 class Project extends React.Component {
+	state = {
+		cardWidth: new Animated.Value(315),
+		cardHeight: new Animated.Value(420),
+		titleTop: new Animated.Value(20),
+		scale: new Animated.Value(0),
+		isCardOpen: false,
+		opacity: new Animated.Value(1),
+	};
+
+	openCard = () => {
+		if (!this.props.canOpen) return;
+		Animated.spring(this.state.cardWidth, {
+			toValue: screenWidth,
+			useNativeDriver: false,
+		}).start();
+		Animated.spring(this.state.cardHeight, {
+			toValue: screenHeight - 80,
+			useNativeDriver: false,
+		}).start();
+		Animated.spring(this.state.titleTop, {
+			toValue: 60,
+			useNativeDriver: false,
+		}).start();
+		Animated.spring(this.state.scale, {
+			toValue: 1,
+			useNativeDriver: false,
+		}).start();
+		Animated.timing(this.state.opacity, {
+			toValue: 0,
+			useNativeDriver: false,
+		}).start();
+
+		this.setState({ isCardOpen: true });
+
+		StatusBar.setHidden(true, true);
+		this.props.openCard();
+	};
+
+	closeCard = () => {
+		Animated.timing(this.state.cardWidth, {
+			toValue: 315,
+			useNativeDriver: false,
+		}).start();
+		Animated.spring(this.state.cardHeight, {
+			toValue: 420,
+			useNativeDriver: false,
+		}).start();
+		Animated.spring(this.state.titleTop, {
+			toValue: 20,
+			useNativeDriver: false,
+		}).start();
+		Animated.spring(this.state.scale, {
+			toValue: 0,
+			useNativeDriver: false,
+		}).start();
+		Animated.timing(this.state.opacity, {
+			toValue: 1,
+			useNativeDriver: false,
+		}).start();
+
+		this.setState({ isCardOpen: false });
+
+		StatusBar.setHidden(false, true);
+		this.props.closeCard();
+	};
+
 	render() {
 		return (
-			<Container>
-				<Cover>
-					<Image source={this.props.image} />
-					<Title>{this.props.title}</Title>
-					<Author>by {this.props.author}</Author>
-				</Cover>
-				<Content>
-					<Text>{this.props.text}</Text>
-				</Content>
-			</Container>
+			<TouchableWithoutFeedback
+				onPress={this.state.isCardOpen ? this.closeCard : this.openCard}
+				// onPress={this.openCard}
+			>
+				<AnimatedContainer
+					style={{ width: this.state.cardWidth, height: this.state.cardHeight }}
+				>
+					<Cover>
+						<Image source={this.props.image} />
+						<AnimatedTitle style={{ top: this.state.titleTop }}>
+							{this.props.title}
+						</AnimatedTitle>
+						<Author>by {this.props.author}</Author>
+						<TouchableOpacity
+							style={{ position: 'absolute', right: 20, top: 20 }}
+							onPress={this.closeCard}
+						>
+							<AnimatedCloseView
+								style={{ transform: [{ scale: this.state.scale }] }}
+							>
+								<Ionicons name="ios-close" size={36} color="#546bfb" />
+							</AnimatedCloseView>
+						</TouchableOpacity>
+					</Cover>
+					<Content>
+						<Text>{this.props.text}</Text>
+						<Animated.View
+							style={{
+								opacity: this.state.opacity,
+								position: 'absolute',
+								bottom: 0,
+								height: '50%',
+								width: '100%',
+							}}
+						>
+							<LinearGradient
+								colors={[
+									'rgba(255,255,255,0)',
+									'rgba(255,255,255,0.75)',
+									'rgba(255,255,255,255)',
+								]}
+								style={{
+									height: '100%',
+									width: '100%',
+								}}
+							/>
+						</Animated.View>
+					</Content>
+				</AnimatedContainer>
+			</TouchableWithoutFeedback>
 		);
 	}
 }
 
-export default Project;
+export default connect(mapStateToProps, mapDispatchToProps)(Project);
 
 const Container = styled.View`
 	width: 315px;
@@ -30,11 +168,12 @@ const Container = styled.View`
 	box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
 `;
 
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
+
 const Cover = styled.View`
-	height: 65%;
+	height: 60%;
 	border-top-left-radius: 14px;
 	border-top-right-radius: 14px;
-	overflow: hidden;
 	overflow: hidden;
 `;
 
@@ -50,8 +189,10 @@ const Title = styled.Text`
 	font-size: 24px;
 	font-weight: bold;
 	color: white;
-	width: 75%;
+	width: 65%;
 `;
+
+const AnimatedTitle = Animated.createAnimatedComponent(Title);
 
 const Author = styled.Text`
 	position: absolute;
@@ -63,12 +204,29 @@ const Author = styled.Text`
 	font-weight: 700;
 `;
 
-const Content = styled.View`
-	height: 35%;
-	/* align-self: baseline; */
+const CloseView = styled.View`
+	/* position: absolute;
+	top: 20px;
+	right: 20px; */
+	width: 36px;
+	height: 36px;
+	border-radius: 22px;
+	background: white;
 	justify-content: center;
+	align-items: center;
+	box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
 `;
 
+const AnimatedCloseView = Animated.createAnimatedComponent(CloseView);
+
+const Content = styled.View`
+	height: 40%;
+	border-bottom-left-radius: 14px;
+	border-bottom-right-radius: 14px;
+	overflow: hidden;
+	/* align-self: baseline; */
+	/* justify-content: center; */
+`;
 const Text = styled.Text`
 	font-size: 17px;
 	margin: 20px;
--- screens/ProjectsScreen.js
+++ screens/ProjectsScreen.js
@@ -2,6 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import Project from '../components/Project';
 import { PanResponder, Animated } from 'react-native';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+	// console.log(state.action);
+	return {
+		action: state.action,
+	};
+}
+
+function changeIndex(index) {
+	if (index + 1 > ProjectArray.length - 1) return 0;
+	return index + 1;
+}
 
 class ProjectsScreen extends React.Component {
 	static navigationOptions = {
@@ -13,10 +26,20 @@ class ProjectsScreen extends React.Component {
 		pan: new Animated.ValueXY(),
 		scale: new Animated.Value(0.9),
 		translateY: new Animated.Value(44),
+		thirdScale: new Animated.Value(0.8),
+		thirdTranslateY: new Animated.Value(-10),
+		index: 0,
+		opacity: new Animated.Value(0),
 	};
 
 	componentWillMount() {
 		this._panResponder = PanResponder.create({
+			onMoveShouldSetPanResponder: (gestureState) => {
+				if (gestureState.dx == 0 && gestureState.dy == 0) return false;
+				else if (this.props.action == 'closeCard') return true;
+				else false;
+			},
+
 			onPanResponderGrant: () => {
 				Animated.spring(this.state.scale, {
 					toValue: 1,
@@ -26,9 +49,21 @@ class ProjectsScreen extends React.Component {
 					toValue: 0,
 					useNativeDriver: false,
 				}).start();
+
+				Animated.spring(this.state.thirdScale, {
+					toValue: 0.9,
+					useNativeDriver: false,
+				}).start();
+				Animated.spring(this.state.thirdTranslateY, {
+					toValue: 44,
+					useNativeDriver: false,
+				}).start();
+				Animated.timing(this.state.opacity, {
+					toValue: 1,
+					useNativeDriver: false,
+				}).start();
 			},
 
-			onMoveShouldSetPanResponder: () => true,
 			onPanResponderMove: Animated.event([
 				null,
 				{ dx: this.state.pan.x, dy: this.state.pan.y },
@@ -36,12 +71,26 @@ class ProjectsScreen extends React.Component {
 
 			onPanResponderRelease: () => {
 				const positionY = this.state.pan.y.__getValue();
+				const positionX = this.state.pan.x.__getValue();
 				console.log(positionY);
-				if (positionY > 250) {
+
+				Animated.timing(this.state.opacity, {
+					toValue: 0,
+					useNativeDriver: false,
+				}).start();
+
+				if (positionY > 150 || positionX > 150) {
 					Animated.timing(this.state.pan, {
-						toValue: { x: this.state.pan.x, y: 1000 },
+						toValue: { x: 0, y: 800 },
 						useNativeDriver: false,
-					}).start();
+					}).start(() => {
+						this.setState({ index: changeIndex(this.state.index) });
+						this.state.pan.setValue({ x: 0, y: 0 });
+						this.state.scale.setValue(0.9);
+						this.state.translateY.setValue(44);
+						this.state.thirdScale.setValue(0.8);
+						this.state.thirdTranslateY.setValue(-10);
+					});
 				} else {
 					Animated.spring(this.state.pan, {
 						toValue: { x: 0, y: 0 },
@@ -55,6 +104,15 @@ class ProjectsScreen extends React.Component {
 						toValue: 44,
 						useNativeDriver: false,
 					}).start();
+
+					Animated.spring(this.state.thirdScale, {
+						toValue: 0.8,
+						useNativeDriver: false,
+					}).start();
+					Animated.spring(this.state.thirdTranslateY, {
+						toValue: -10,
+						useNativeDriver: false,
+					}).start();
 				}
 			},
 		});
@@ -63,6 +121,7 @@ class ProjectsScreen extends React.Component {
 	render() {
 		return (
 			<Container>
+				<AnimatedMask style={{ opacity: this.state.opacity }} />
 				<Animated.View
 					style={{
 						transform: [
@@ -73,10 +132,11 @@ class ProjectsScreen extends React.Component {
 					{...this._panResponder.panHandlers}
 				>
 					<Project
-						title={ProjectArray[0].title}
-						image={ProjectArray[0].image}
-						author={ProjectArray[0].author}
-						text={ProjectArray[0].text}
+						title={ProjectArray[this.state.index].title}
+						image={ProjectArray[this.state.index].image}
+						author={ProjectArray[this.state.index].author}
+						text={ProjectArray[this.state.index].text}
+						canOpen={true}
 					/>
 				</Animated.View>
 				<Animated.View
@@ -95,17 +155,39 @@ class ProjectsScreen extends React.Component {
 					}}
 				>
 					<Project
-						title={ProjectArray[1].title}
-						image={ProjectArray[1].image}
-						author={ProjectArray[1].author}
-						text={ProjectArray[1].text}
+						title={ProjectArray[changeIndex(this.state.index)].title}
+						image={ProjectArray[changeIndex(this.state.index)].image}
+						author={ProjectArray[changeIndex(this.state.index)].author}
+						text={ProjectArray[changeIndex(this.state.index)].text}
+					/>
+				</Animated.View>
+				<Animated.View
+					style={{
+						position: 'absolute',
+						top: 0,
+						width: '100%',
+						height: '100%',
+						zIndex: -2,
+						justifyContent: 'center',
+						alignItems: 'center',
+						transform: [
+							{ scale: this.state.thirdScale },
+							{ translateY: this.state.thirdTranslateY },
+						],
+					}}
+				>
+					<Project
+						title={ProjectArray[changeIndex(this.state.index + 1)].title}
+						image={ProjectArray[changeIndex(this.state.index + 1)].image}
+						author={ProjectArray[changeIndex(this.state.index + 1)].author}
+						text={ProjectArray[changeIndex(this.state.index + 1)].text}
 					/>
 				</Animated.View>
 			</Container>
 		);
 	}
 }
-export default ProjectsScreen;
+export default connect(mapStateToProps)(ProjectsScreen);
 
 const Container = styled.View`
 	flex: 1;
@@ -113,6 +195,18 @@ const Container = styled.View`
 	align-items: center;
 	background: #f0f3f5;
 `;
+
+const Mask = styled.View`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	z-index: -3;
+	background: rgba(0, 0, 0, 0.25);
+`;
+
+const AnimatedMask = Animated.createAnimatedComponent(Mask);
 
 const ProjectArray = [
 	{
@@ -127,13 +221,13 @@ const ProjectArray = [
 		image: require('../assets/background12.jpg'),
 		author: 'Falah Nurli Filano',
 		text:
-			'This is an placeholder text. usually it goes with Lorem Ipsum or other generated text for text filled space.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
 	},
 	{
-		title: 'Advanced Animation and Transition',
-		image: require('../assets/background7.jpg'),
+		title: 'Animation and Transition',
+		image: require('../assets/background4.jpg'),
 		author: 'Falah Nurli Filano',
 		text:
-			'This is an placeholder text. usually it goes with Lorem Ipsum or other generated text for text filled space.',
+			'Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a first-century BC text by the Roman statesman. Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a first-century BC text by the Roman statesman. Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a first-century BC text by the Roman statesman. Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a first-century BC text by the Roman statesman.',
 	},
 ];
